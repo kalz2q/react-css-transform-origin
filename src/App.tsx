9256d9912e1806bc8ec5d367a3011d1251e797b9
kalz2q <katsu95i@gmail.com>
2022-09-28 02:20:30 +0900

    m

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,19 @@ function App() {
   })
   return (
     <div className="App">
-      <div className="box"></div>
+      <div className="wrapper">
+        <div className="container">
+          <div className="box box1">500ms</div>
+        </div>
+        <div className="container">
+          <div className="box box2">1s</div>
+        </div>
+        <div className="container">
+          <div className="box box3">4s</div>
+        </div>
+
+
+      </div>
     </div>
   );
 }
